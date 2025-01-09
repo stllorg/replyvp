ac12b410f2a83d49824b5dd7fd4a4058acd32607
samuelmendespy <75790706+samuelmendespy@users.noreply.github.com>
2025-01-08 22:49:34 -0300

    feat(api): add GET method

---
+++ README.md
@@ -0,0 +1,7 @@
+Currently, a simple PHP API has been designed to handle sending messages to a PostgreSQL database.
+
+## Requirements
+
+- **API**: PHP 8.2 with Apache 2.4 and MySQLi
+- **Banco de Dados**: MySQL v9.1
+- **Containers**: Docker Compose
--- web/api/index.php
+++ web/api/index.php
@@ -11,20 +11,21 @@ try {
 
         if (!isset($input['message']) || strlen($input['message']) > 250) {
             http_response_code(400);
-            echo json_encode(["error" => "The message is invalid or too long."]);
+            echo json_encode(["error" => "The message is invalid or too long."], JSON_UNESCAPED_UNICODE);
             exit;
         }
 
         $stmt = $connection->prepare("INSERT INTO messages (content) VALUES (?)");
         $stmt->bind_param('s', $input['message']);
         $stmt->execute();
-
-        echo json_encode(["success" => true, "id" => $connection->insert_id]);
+        
+        http_response_code(201);
+        echo json_encode(["success" => true, "id" => $connection->insert_id], JSON_UNESCAPED_UNICODE);
     } else {
         http_response_code(405);
         echo json_encode(["error" => "405 Method not allowed."]);
     }
 } catch (Exception $e) {
     http_response_code(500);
-    echo json_encode(["error" => "Erro no servidor.", "details" => $e->getMessage()]);
+    echo json_encode(["error" => "Server error.", "details" => $e->getMessage()], JSON_UNESCAPED_UNICODE);
 }

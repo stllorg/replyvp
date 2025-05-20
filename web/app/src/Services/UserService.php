741fd90e5adfb89bdaa3458e344085c4304677e5
samuelmendespy <samuelmendespy@gmail.com>
2025-05-20 15:28:27 -0300

    feat: create user service

---
+++ web/app/src/Services/UserService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ReplyVP\Services;
+
+use ReplyVP\Entities\User;
+use ReplyVP\Repositories\UserRepository;
+
+class UserService {
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository) {
+    }
+
+    public function createUser($user): User {
+        return $this->userRepository->create($user);
+    }
+
+    public function getUserById($userId): ?User {
+        return $this->userRepository->findById($userId);
+    }
+
+    public function getUserByUsername(string $userName): ?User {
+        return $this->userRepository->findByUsername($userName);
+    }
+
+    public function getUserByEmail(string $email): ?User {
+        return $this->userRepository->findByEmail($email);
+    }
+
+    public function getUserRoles($userId): array {
+        return this->userRepository->findRolesByUserId($userId);
+    }
+} 

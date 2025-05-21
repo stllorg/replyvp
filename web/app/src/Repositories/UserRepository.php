5948f26c90efc62e1c0dded2c780d6436a9ce126
samuelmendespy <samuelmendespy@gmail.com>
2025-05-21 07:15:53 -0300

    feat: add transaction to create user with default role

--- web/app/src/Repositories/UserRepository.php
+++ web/app/src/Repositories/UserRepository.php
@@ -12,17 +12,58 @@ class UserRepository {
     }
 
     public function create(User $user): User {
-        $stmt = $this->db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-        $username = $user->getUsername();
-        $email = $user->getEmail();
-        $password = $user->getPassword();
-        $stmt->bind_param("sss", $username, $email, $password);
-        $stmt->execute();
-        $user->setId($stmt->insert_id);
-        return $user;
+        $this->db->autocommit(FALSE);
+
+        try {
+            $stmt = $this->db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+            if (!$stmt) {
+                throw new Exception("Database prepare failed for user: " . $this->db->error);
+            }
+
+            $username = $user->getUsername();
+            $email = $user->getEmail();
+            $password = $user->getPassword();
+            
+            $stmt->bind_param("sss", $username, $email, $password);
+            $stmt->execute();
+
+            if ($stmt->error) {
+                throw new Exception("Failed to insert new user: " . $stmt->error);
+            }
+
+            $user->setId($stmt->insert_id);
+            $stmt->close();
+
+            $userId = $user->getId();
+
+            $stmtRole = $this->db->prepare("INSERT INTO user_roles (user_id, role_id) VALUES (?, 4)");
+
+            if (!$stmtRole) {
+                throw new Exception("Database prepare failed for role data: " . $this->db->error);
+            }
+
+            $stmtRole->bind_param("i", $userId);
+
+            $stmtRole->execute();
+
+            if ($stmtRole->error) {
+                throw new Exception("Failed to create Role data: " . $stmtRole->error);
+            }
+
+            $stmtRole->close();
+
+            $this->db->commit();
+
+            return $user;
+        }
+        catch (Exception $e) {
+            $this->db->rollback();
+            error_log("Error creating user: " . $e->getMessage());
+            throw $e;
+        }
     }
 
-    public function findByEmail($email): User {
+    public function findByEmail($email): ?User {
         $stmt = $this->db->prepare("SELECT id, username, email, password FROM users WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();

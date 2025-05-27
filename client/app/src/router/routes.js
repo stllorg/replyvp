3431f1f9152dbb1043882d6912afcfb6884f1fa0
samuelmendespy <samuelmendespy@gmail.com>
2025-05-27 15:11:07 -0300

    refactor: remove pages

--- client/app/src/router/routes.js
+++ client/app/src/router/routes.js
@@ -1,12 +1,9 @@
-
-// Public
 import HomePage from '../views/HomePage.vue';
 import ContactUs from '@/views/ContactUs.vue';
 import TermsPage from '@/views/TermsPage.vue';
 import PrivacyPolicyPage from '@/views/PrivacyPolicyPage.vue';
 import NotFoundPage from '@/views/NotFoundPage.vue';
 
-// Logged users
 import DashboardPage from '@/views/DashboardPage.vue';
 
 // User
@@ -15,16 +12,7 @@ import TerminateAccount from '@/views/user/TerminateAccount.vue';
 import UpdateUserPage from '@/views/user/UpdateUserPage.vue';
 import MessagesPage from '@/views/user/MessagesPage.vue';
 
-// Support
-import PendingTicketsList from '@/views/support/PendingTicketsList.vue';
-import ReplyTicketPage from '@/views/support/ReplyTicketPage.vue';
-
-// Ticket History & Search 
-import TicketSearchPage from '@/components/pages/tickets/TicketSearchPage.vue';
-import TicketHistoryPage from '@/components/pages/tickets/TicketHistoryPage.vue';
-
 // Admin
-import FinishedList from '@/views/admin/FinishedList.vue';
 import AdminRolesPage from '@/views/admin/AdminRolesPage.vue';
 
 export const routes = [
@@ -35,14 +23,9 @@ export const routes = [
   { path: '/register', redirect: '/'},
   { path: '/policy', name: 'PrivacyPolicyPage', component: PrivacyPolicyPage},
   { path: '/terms', name: 'TermsPage', component: TermsPage},
-  { path: '/dashboard', name: 'DashboardPage', component: DashboardPage, meta: { requiresAuth: true }},
-  { path: '/tickets/search', name: 'TicketSearchPage', component: TicketSearchPage, meta: {requiresAuth: true }},
-  { path: '/tickets/history', name: 'TicketHistoryPage', component: TicketHistoryPage, meta: {requiresAuth: true }},
-  { path: '/admin/finished', name: 'FinishedList', component: FinishedList, meta: {requiresAdmin: true }},
-  { path: '/admin/roles', name: 'AdminRolesPage', component: AdminRolesPage, meta: {requiresAdmin: true }},
-  { path: '/support/pending', name: 'PendingTicketsList', component: PendingTicketsList, meta: {requiresSupport: true }},
-  { path: '/support/reply', name: 'ReplyTicketPage', component: ReplyTicketPage, meta: {requiresSupport: true }},
   { path: '/user/terminate', name: 'TerminateAccount', component: TerminateAccount},
+  { path: '/dashboard', name: 'DashboardPage', component: DashboardPage, meta: { requiresAuth: true }},
+  { path: '/admin/roles', name: 'AdminRolesPage', component: AdminRolesPage, meta: {requiresAdmin: true }},
   { path: '/user/update', name: 'UpdateUserRegistrationPage', component: UpdateUserPage, meta: { requiresAuth: true }},
   { path: '/user/tickets/new', name: 'NewTicketPage', component: NewTicketPage },
   { path: '/user/messages', name: 'MessagesPage', component: MessagesPage, meta: { requiresAuth: true }},

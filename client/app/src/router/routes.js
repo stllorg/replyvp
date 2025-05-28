2ff148dfca34de123d93310a0223825adadac362
samuelmendespy <samuelmendespy@gmail.com>
2025-05-27 21:02:15 -0300

    refactor: remove pages

--- client/app/src/router/routes.js
+++ client/app/src/router/routes.js
@@ -1,5 +1,4 @@
 import HomePage from '../views/HomePage.vue';
-import ContactUs from '@/views/ContactUs.vue';
 import TermsPage from '@/views/TermsPage.vue';
 import PrivacyPolicyPage from '@/views/PrivacyPolicyPage.vue';
 import NotFoundPage from '@/views/NotFoundPage.vue';
@@ -18,15 +17,14 @@ import AdminRolesPage from '@/views/admin/AdminRolesPage.vue';
 export const routes = [
   { path: '/', name: 'HomePage', component: HomePage },
   { path: '/:catchAll(.*)', name: 'NotFoundPage', component: NotFoundPage},
-  { path: '/contact', name: 'ContactUs', component: ContactUs},
   { path: '/login', redirect: '/'},
   { path: '/register', redirect: '/'},
   { path: '/policy', name: 'PrivacyPolicyPage', component: PrivacyPolicyPage},
   { path: '/terms', name: 'TermsPage', component: TermsPage},
-  { path: '/user/terminate', name: 'TerminateAccount', component: TerminateAccount},
   { path: '/dashboard', name: 'DashboardPage', component: DashboardPage, meta: { requiresAuth: true }},
   { path: '/admin/roles', name: 'AdminRolesPage', component: AdminRolesPage, meta: {requiresAdmin: true }},
   { path: '/user/update', name: 'UpdateUserRegistrationPage', component: UpdateUserPage, meta: { requiresAuth: true }},
   { path: '/user/tickets/new', name: 'NewTicketPage', component: NewTicketPage },
   { path: '/user/messages', name: 'MessagesPage', component: MessagesPage, meta: { requiresAuth: true }},
+  { path: '/user/terminate', name: 'TerminateAccount', component: TerminateAccount},
 ];

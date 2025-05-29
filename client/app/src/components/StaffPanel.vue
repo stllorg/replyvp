1248c3888a7a9d074edf35f31d1f9bd6e44dbad7
samuelmendespy <samuelmendespy@gmail.com>
2025-05-28 22:21:03 -0300

    feat: add component staff panel

---
+++ client/app/src/components/StaffPanel.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card shadow mb-4 border-0" style="background-color: #f8f9fa">
+        <h1 class="fs-2 fs-sm-3 fw-bold text-center text-secondary mb-4">Staff Area</h1>
+        <StaffUtilsTray />
+        <br>
+    </div>
+</template>
+
+<script setup>
+import StaffUtilsTray from './StaffUtilsTray.vue';
+</script>

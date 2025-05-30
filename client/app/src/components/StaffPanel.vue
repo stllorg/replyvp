5bc08eb93395c7107e325ee1326579cefe713fc3
samuelmendespy <samuelmendespy@gmail.com>
2025-05-30 14:24:26 -0300

    style: adjust code style

--- client/app/src/components/StaffPanel.vue
+++ client/app/src/components/StaffPanel.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="card shadow mb-4 border-0" style="background-color: #f8f9fa">
-        <h1 class="fs-2 fs-sm-3 fw-bold text-center text-secondary mb-4">Staff Area</h1>
-        <StaffUtilsTray @display-area="emitDisplayArea"/>
-        <br>
-    </div>
+  <div class="card shadow mb-4 border-0" style="background-color: #f8f9fa">
+    <h1 class="fs-2 fs-sm-3 fw-bold text-center text-secondary mb-4">
+      Staff Area
+    </h1>
+    <StaffUtilsTray @display-area="emitDisplayArea" />
+    <br />
+  </div>
 </template>
 
 <script setup>
-import { defineEmits } from 'vue';
-import StaffUtilsTray from './StaffUtilsTray.vue';
+import { defineEmits } from "vue";
+import StaffUtilsTray from "./StaffUtilsTray.vue";
 
-const emit = defineEmits(['display-area']);
+const emit = defineEmits(["display-area"]);
 
 const emitDisplayArea = (areaName) => {
-  emit('display-area', areaName);
+  emit("display-area", areaName);
 };
 </script>
--- client/app/src/views/admin/AdminRolesPage.vue
+++ client/app/src/views/admin/AdminRolesPage.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container py-5">
     <div class="card shadow mb-4 border-0" style="background-color: #f8f9fa">
-    <AccountsBoard />
-  </div>
+      <AccountsBoard />
+    </div>
   </div>
 </template>
 
 <script setup>
 import AccountsBoard from "@/components/AccountsBoard.vue";
-
 </script>

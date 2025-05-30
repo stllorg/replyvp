d0ccaa1f1eeff5018bf73eb4ee16244ef1422d2b
samuelmendespy <samuelmendespy@gmail.com>
2025-05-30 18:34:32 -0300

    fix: correct typo

--- client/app/src/services/userService.js
+++ client/app/src/services/userService.js
@@ -67,7 +67,7 @@ export async function updateUserRoleById(userId) {
       return response;
     }
 
-  } catch (err) {
+  } catch (error) {
     console.error("Erro ao obter usuário:", error);
     throw error;
   }

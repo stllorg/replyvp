eec8c27d6e0688703f5df5dcc8c1e453a1b2fcbe
samuelmendespy <samuelmendespy@gmail.com>
2025-05-31 09:06:00 -0300

    feat: detach staff area to a new page

--- client/app/src/views/StaffAreaPage.vue
+++ client/app/src/views/StaffAreaPage.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container mt-4">
+    <StaffPanel v-if="showStaffContent" @display-area="handleDisplayArea" />
     <LoadingComponent v-if="loading" />
-    <GenerateTicketComponent v-if="showUserContent" />
     <TicketsBoard
-      v-if="showUserContent"
-      :boardTitle="boardsTitles.userTickets"
-      :tickets="userTicketsList"
+      v-if="displayArea === 'pending' && showStaffContent"
+      :boardTitle="boardsTitles.pendingTickets"
+      :tickets="pendingTicketsList"
+      @view-messages="goToMessages"
+      @archive-ticket="archiveTicket"
+    />
+    <InteractionsBoard
+      v-if="displayArea === 'search' && showStaffContent"
+      :boardTitle="boardsTitles.interactions"
+      :interactions="interactionsList"
       @view-messages="goToMessages"
       @archive-ticket="archiveTicket"
     />
@@ -14,24 +21,30 @@
 
 <script setup>
 import ticketService from "@/services/ticketService";
+import { getTicketsWithUserMessages } from "@/services/userService";
 import { useToast } from "vue-toastification";
 import { useRouter } from "vue-router";
 import { ref, onMounted } from "vue";
 import { useAuthStore } from "@/stores/authStore";
+import { computed } from "vue";
+import InteractionsBoard from "@/components/InteractionsBoard.vue";
 import TicketsBoard from "@/components/TicketsBoard.vue";
+import StaffPanel from "@/components/StaffPanel.vue";
 import LoadingComponent from "@/components/LoadingComponent.vue";
-import GenerateTicketComponent from "@/components/GenerateTicketComponent.vue";
 
 const router = useRouter();
 const authStore = useAuthStore();
 const toast = useToast();
 
-const showUserContent = ref(false);
-const userTicketsList = ref([]);
+const user = authStore.user;
+const interactionsList = ref([]);
+const pendingTicketsList = ref([]);
+
 const displayArea = ref(null);
 const loading = ref(false);
 const boardsTitles = {
-  userTickets: "Meus tickets",
+  pendingTickets: "Tickets de casos abertos",
+  interactions: "Histórico de Interações",
 };
 
 onMounted(async () => {
@@ -39,14 +52,8 @@ onMounted(async () => {
     if (!authStore.isUserLogged) {
       toast.error("Falha na autenticação!", { timeout: 3000 });
       navigateToHome();
-      return;
     }
 
-    if (authStore.isRegularUser) {
-
-      showUserContent.value = true;
-      handleDisplayArea("userArea");
-    }
   } catch (err) {
     console.log(err);
     toast.error("Ocorreu um erro ao conectar com o servidor!", {
@@ -72,19 +79,37 @@ const archiveTicket = (id) => {
   console.log("No logic to archive ticket:", id);
 };
 
+const showStaffContent = computed(() =>
+  ["admin", "manager", "support"].some((role) => user.roles.includes(role))
+);
+
 const handleDisplayArea = async (areaName) => {
   loading.value = true;
   displayArea.value = null;
 
-  if (areaName === "userArea") {
+  if (areaName === "pending") {
     try {
-      const response = await ticketService.getTickets();
+      const response = await ticketService.getPendingTickets();
       if (response.status != 200) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      loadUserData(response.data);
-      displayArea.value = "userArea";
+      loadPendingTickets(response.data);
+      displayArea.value = "pending";
+    } catch (err) {
+      console.log("Failed to fetch data:", err);
+    } finally {
+      loading.value = false;
+    }
+  } else if (areaName === "search") {
+    try {
+      const response = await getTicketsWithUserMessages();
+
+      if (response.status != 200) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      loadInteractionsData(response.data);
+      displayArea.value = "search";
     } catch (err) {
       console.log("Failed to fetch data:", err);
     } finally {
@@ -98,7 +123,20 @@ const handleDisplayArea = async (areaName) => {
   }
 };
 
-const loadUserData = (data = []) => {
+const loadPendingTickets = (data = []) => {
+  data.forEach((item) => {
+    const retrievedUserTicket = {
+      id: item.id,
+      subject: item.subject,
+      status: item.status,
+      createdAt: item.createdAt,
+    };
+
+    pendingTicketsList.value.push(retrievedUserTicket);
+  });
+};
+
+const loadInteractionsData = (data = []) => {
   data.forEach((item) => {
     const statusMap = {
       open: "Aberta",
@@ -110,13 +148,12 @@ const loadUserData = (data = []) => {
       id: item.id,
       subject: item.subject,
       status: statusMap[item.status] || "Desconhecido",
-      createdAt: item.createdAt,
+      createdAt: item.createdAt ?? new Date().toISOString(),
     };
 
-    userTicketsList.value.push(retrievedUserTicket);
+    interactionsList.value.push(retrievedUserTicket);
   });
 };
-
 </script>
 
 <style>
